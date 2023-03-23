07997a31eca2d3838e75550d3cb669d027536acb
Abhijeet Go-kar <abunickabhi@gmail.com>
2023-03-23 15:09:37 +0530

    changes

--- 4_Simulation_8_bit_Counter/counter.vhd
+++ 4_Simulation_8_bit_Counter/counter.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03.12.2019 18:14:39
--- Design Name: 
--- Module Name: counter - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
  use IEEE.std_logic_arith.all;
--- 4_Simulation_8_bit_Counter/testbench.vhd
+++ 4_Simulation_8_bit_Counter/testbench.vhd
@@ -1,25 +1,3 @@
-
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 17.12.2019 01:58:29
--- Design Name: 
--- Module Name: counter_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
